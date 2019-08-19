20bfa3766825e4167fa85787477d4b0965111fb1
CLAMON-8244LRQ\Kristoffer Hemlin <kristoffer.hemlin@claremont.se>
2019-08-19 14:30:28 +0200

    Initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+// import Claremont from './img/claremont.svg'
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="background">
+      <h3>Hallå där</h3>
+    </div>
+  );
+}
+
+export default Header;
